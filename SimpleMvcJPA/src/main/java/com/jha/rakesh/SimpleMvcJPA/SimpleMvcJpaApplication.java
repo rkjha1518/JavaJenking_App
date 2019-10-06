dda5d65ee2b113c40b0a1eac991d90f100005b8c
rakesh <rakjha1518@gmail.com>
2019-10-06 10:58:46 +0530

    First Message

---
+++ SimpleMvcJPA/src/main/java/com/jha/rakesh/SimpleMvcJPA/SimpleMvcJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jha.rakesh.SimpleMvcJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMvcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMvcJpaApplication.class, args);
+	}
+
+}
---
+++ SimpleMvcJPA/src/test/java/com/jha/rakesh/SimpleMvcJPA/SimpleMvcJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jha.rakesh.SimpleMvcJPA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleMvcJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
